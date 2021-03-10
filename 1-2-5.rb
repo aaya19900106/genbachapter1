1cdc995c07c520a762d89d2b20524fe95a2e596c
aaya19900106 <katsuya@startup-technology.com>
2021-03-11 00:35:25 +0900

    test

---
+++ 1-2-5.rb
@@ -0,0 +1,8 @@
+class Myclass
+  def method_1
+    @number = 100
+  end
+  def method_2
+    @number
+  end
+end
--- 1-2.rb
+++ 1-2.rb
@@ -1,4 +1,8 @@
 class User
-  def  name
+  def  name=(name)
+    @name = name
+  end
+  def name
+    @name
   end
 end
